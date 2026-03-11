724dbe38da8b08e6aef507aa7caaa34c5e67266e
Vansh Singla <vanshsingla@Vanshs-MacBook-Air-4.local>
2026-03-11 21:46:09 +0530

    firereach

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://127.0.0.1:5001/run-agent"
+const API_URL = "https://firereach-l633.onrender.com/run-agent"
 
 export const runAgent = async(data)=>{
 

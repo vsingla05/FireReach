b491505466b8ceefc7039655f0a4a5336e4504d5
Vansh Singla <vanshsingla@Vanshs-MacBook-Air-4.local>
2026-03-28 01:19:38 +0530

    adding docker-file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "https://firereach-l633.onrender.com/run-agent"
+const API_URL = import.meta.env.VITE_API_URL || "https://firereach-l633.onrender.com/run-agent"
 
 export const runAgent = async(data)=>{
 
